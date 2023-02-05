9b351a3f16c19bd9764c04cec957f79c9e023812
Joe Niemiec <joe.niemiec@gmail.com>
2023-02-04 21:16:59 -0600

    Got the data flow for auth functioning

--- pages/api/auth.js
+++ pages/api/auth.js
@@ -1,4 +1,5 @@
 import { createServerSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { exchangeAuthCode } from "../../lib/stravaUtils"
 
 // This is the API triggered when someone connects to Strava
 export default async function handler(req, res) {
@@ -13,42 +14,30 @@ export default async function handler(req, res) {
     data: { session },
   } = await supabase.auth.getSession()
 
-  console.log(session)
-
   if (!session)
     return res.status(401).json({
       error: 'not_authenticated',
       description: 'The user does not have an active session or is not authenticated',
     })
 
-  // Run queries with RLS on the server
-  const { data } = await supabase
-    .from('profiles')
-    .select('strava_owner_id')
-    .eq('id', session.user.id)
-  //res.json(data)
-  
+  //If you have a code do the post request to strava for keys
+  let stravaData = await exchangeAuthCode(code)
 
-  console.log(data[0].strava_owner_id)
-
-  console.log(state)
-  console.log(code)
-  console.log(scope)
   if (req.query?.error) {
     // Process a POST request
     console.log("Error")
   } else {
-    //Take the code and do a POST request. Store in DB.
-    // const error = await supabase
-    //   .from('profiles')
-    //   .update({ code })
-    //   .eq('strava_owner_id', stravaOwnerID)
+    // Update user profile with code,state,scope from query 
+    const error = await supabase
+      .from('profiles')
+      .update({ code, state, scope, 'strava_owner_id': stravaData.athlete.id, 'strava_access_token': stravaData.accessToken, 'strava_refresh_token': stravaData.refreshToken })
+      .eq('id', session.user.id)
 
-    // console.log(error)
+    console.log(error)
   }
 
   // Store Code in DB and set a flag or something that lets the app know I'm connected for this user!
   res.writeHead(302, { Location: '/' });
   res.end();
-  
+
 }
